1effaf7cea848c33d3d291f8fba4ed9db1d631ed
Josip Torić <masperado@gmail.com>
2017-01-12 18:11:15 +0100

    Small changes for login

--- src/ChessSharp/Controllers/HomeController.cs
+++ src/ChessSharp/Controllers/HomeController.cs
@@ -2,15 +2,35 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ChessSharp.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ChessSharp.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public HomeController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
         public IActionResult Index()
         {
+            if (User.Identity.IsAuthenticated)
+            {
+                return RedirectToAction("Profile");
+            }
             return View();
+        }
+
+        public async Task<IActionResult> Profile()
+        {
+            var user = await _userManager.GetUserAsync(User);
+
+            return View(user);
         }
 
         public IActionResult About()
